2cb891b29e07f48438d1b95103be29c5d0ae9d02
Rachel Peng <rachelvpeng@gmail.com>
2021-05-04 15:52:12 -0400

    positions

---
+++ GUI/GUIPosition.java
@@ -0,0 +1,40 @@
+package GUI;
+
+import Objects.Position;
+import Types.TransactionType;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIPosition extends JPanel {
+    JPanel panel;
+
+    public GUIPosition(Position position) {
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(1, 4));
+
+        JLabel stock = new JLabel(position.getStock().getSymbol());
+        panel.add(stock);
+
+        JLabel quantity = new JLabel(position.getQuantity() + "");
+        panel.add(quantity);
+
+        JLabel totalCost = new JLabel(position.getTotalCost() + "");
+        panel.add(totalCost);
+
+        double unrealizedPL = position.getUnrealizedPL();
+        JLabel unrealizedPLLabel;
+        if (unrealizedPL < 0) {
+            unrealizedPLLabel = new JLabel("-$" + unrealizedPL);
+            unrealizedPLLabel.setForeground(Color.RED);
+        } else {
+            unrealizedPLLabel = new JLabel("$" + unrealizedPL);
+            unrealizedPLLabel.setForeground(Color.GREEN);
+        }
+        panel.add(unrealizedPLLabel);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+}
--- GUI/GUIStockPositions.java
+++ GUI/GUIStockPositions.java
@@ -12,24 +12,35 @@ import java.util.ArrayList;
 
 public class GUIStockPositions extends JPanel{
     JPanel panel;
+    JSplitPane sp;
 
     public GUIStockPositions(Person person) {
-        System.out.println(person.getSecurityAccount());
         ArrayList<Position> positions = person.getSecurityAccount().getPositions();
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(positions.size(), 3));
+        panel.setLayout(new BorderLayout());
 
+        sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        sp.setResizeWeight(0.1);
+
+        JPanel topPanel = new JPanel();
+        topPanel.setBounds(25, 25, 400, 500);
+
+        JPanel titlePanel = new JPanel(new GridLayout(1, 4));
+        titlePanel.add(new JLabel("Stock"));
+        titlePanel.add(new JLabel("Quantity"));
+        titlePanel.add(new JLabel("Total Cost"));
+        titlePanel.add(new JLabel("Unrealized Profit / Loss"));
+        sp.add(titlePanel);
+
+        JPanel positionsPanel = new JPanel(new GridLayout(positions.size(), 1));
         for (Position p : positions) {
-            JLabel stock = new JLabel(p.getStock().getSymbol());
-            panel.add(stock);
-
-            JLabel quantity = new JLabel(p.getQuantity() + "");
-            panel.add(quantity);
-
-            JLabel totalCost = new JLabel(p.getTotalCost() + "");
-            panel.add(totalCost);
+            positionsPanel.add((new GUIPosition(p)).getPanel());
         }
+        sp.add(positionsPanel);
+
+        panel.add(sp, BorderLayout.CENTER);
+        panel.setAlignmentX(Component.LEFT_ALIGNMENT);
     }
 
     public JPanel getPanel(){
