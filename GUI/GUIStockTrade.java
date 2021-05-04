54175af6e6783ff8406ddf2342a68d779ab8fb70
Yuan Wei <yuanwei@bu.edu>
2021-05-04 16:54:41 -0400

    half baked

--- GUI/GUIStockTrade.java
+++ GUI/GUIStockTrade.java
@@ -4,28 +4,63 @@ import Objects.Person;
 import Objects.Position;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 
 public class GUIStockTrade extends JPanel {
     JPanel panel;
     JSplitPane sp;
 
-    JTextField loanAmountTF, paymentAmountTF;
-    JLabel originalLoanAmountLabel, loanBalanceLabel, errorLabel, loanPaidLabel, accountChoiceLabel, paymentAmountLabel;
-    JButton submitBtn;
-    JCheckBox termsCB, interestTermsCB;
+    JButton buyBtn, sellBtn;
+    JTextField stockNameText;
+    JTextField stockSymbolText;
+    JTextField requestAmountText;
+
+
+
     JComboBox<String> accountCB;
 
     public GUIStockTrade(Person person) {
         ArrayList<Position> positions = person.getSecurityAccount().getPositions();
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 4));
-        submitBtn = new JButton();
-        submitBtn.setText("??????");
-        submitBtn.setVisible(true);
-        panel.add(submitBtn);
+        JLabel userLabel = new JLabel("Enter The stock name: ");
+        userLabel.setBounds(100, 200, 150, 25);
+        panel.add(userLabel);
+
+        JLabel symbolLabel = new JLabel("Enter The stock symbol: ");
+        symbolLabel.setBounds(100, 230, 150, 25);
+        panel.add(symbolLabel);
+
+        JLabel amountLabel = new JLabel("Enter the amount: ");
+        amountLabel.setBounds(100, 260, 150, 25);
+        panel.add(amountLabel);
+
+        stockNameText = new JTextField(20);
+        stockNameText.setBounds(300, 200, 165, 25);
+        panel.add(stockNameText);
+
+        stockSymbolText = new JTextField(20);
+        stockSymbolText.setBounds(300, 230, 165, 25);
+        panel.add(stockSymbolText);
+
+        requestAmountText = new JTextField(20);
+        requestAmountText.setBounds(300, 260, 165, 25);
+        panel.add(requestAmountText);
+
+        panel.setLayout(null);
+        buyBtn = new JButton("BUY");
+        buyBtn.setBounds(180, 310, 80, 40);
+
+        sellBtn = new JButton("SELL");
+        sellBtn.setBounds(270,310,80, 40);
+
+
+
+        sellBtn.setVisible(true);
+        buyBtn.setVisible(true);
+
+        panel.add(buyBtn);
+        panel.add(sellBtn);
     }
 
     public JPanel getPanel() {
